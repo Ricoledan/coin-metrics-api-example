f6019c1e796db3a0454bcb75319fd01ce15a3bf2
Ricardo Ledan <ricoledan@gmail.com>
2023-05-23 12:56:56 -0400

    misc

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from coinmetrics.api_client import CoinMetricsClient
 load_dotenv()
 
 client = CoinMetricsClient(os.getenv('COIN_METRICS_API_KEY'))
-
 data = client.catalog_markets().to_dataframe()
 
 print(data)
